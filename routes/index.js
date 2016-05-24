280e1e05a1113a477f7e019b805820ea9d2b769d
Nan <nano.mv112@gmail.com>
2016-05-24 14:52:08 -0700

    login post /login

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,6 @@ router.get('/', function(req, res, next) {
   else res.render("index");
 });
 
-router.post('/', function(req, res, next) {
-  console.log(req.body);
-  if(req.body.email && req.body.password){
-    req.session.email = req.body.email;
-    req.session.password = req.body.password;
-    res.render("index");
 
-  }else res.send("Error: email or password not found");
-});
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -6,10 +6,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   //when route has ":param", req.params.param is available
   if(req.session.email && req.session.password)
-    res.redirect("..");
+    res.redirect("/");
   else res.render('login');
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  if(req.body.email && req.body.password){
+    req.session.email = req.body.email;
+    req.session.password = req.body.password;
+    res.redirect("/");
 
+  }else res.send("Error: email or password not found");
+});
 
 module.exports = router;
