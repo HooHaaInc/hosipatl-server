36b590117c5a4e804a32282548cfeb79652776a1
Nan <nano.mv112@gmail.com>
2016-05-25 10:37:51 -0700

    tabla pacientes

--- routes/tablepacientes.js
+++ routes/tablepacientes.js
@@ -4,7 +4,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
   //TODO: SELECT FROM Pacientes
-  req.app.mysql.query("SELECT * FROM Usuario", function(err, rows, fields){
+  var query = "SELECT pa.id_Paciente AS Id, pe.nombre AS Nombre, " +
+  "pe.apellido_paterno AS 'Apellido Paterno', pe.apellido_materno AS 'Apellido Materno', " +
+  "pa.entidad_serv_salud AS 'Entidad de Servicio de Saludo', pa.eps AS EPS FROM Paciente pa INNER JOIN Persona pe " +
+  "ON pa.id_Persona = pe.id_Persona";
+  req.app.mysql.query(query, function(err, rows, fields){
+    if(err){
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+      return;
+    }
     var locals = {
       query: { rows: rows, fields: fields, id: "id_Paciente" },
       href: "/viewpaciente",
