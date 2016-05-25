b0c8c12146caebddd155b1e770bfe62e0ca35661
Ernesto Jaramillo <nethojs29@gmail.com>
2016-05-25 10:43:58 -0700

    Medicos/Doctores ya se crean y se ven wapos:3

--- routes/tabledoctores.js
+++ routes/tabledoctores.js
@@ -4,10 +4,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
   //TODO: SELECT FROM Pacientes
-  req.app.mysql.query("SELECT * FROM Usuario", function(err, rows, fields){
+  var query = "SELECT me.id_Medico AS Id, pe.nombre AS Nombre, " +
+  "pe.apellido_paterno AS 'Apellido Paterno', pe.apellido_materno AS 'Apellido Materno', " +
+  "me.especialidad AS 'Especialidad', me.cedula_profesional AS 'Cedula Profesional' FROM Medico me INNER JOIN Persona pe " +
+  "ON me.id_Persona = pe.id_Persona";
+  req.app.mysql.query(query, function(err, rows, fields){
     var locals = {
       query: { rows: rows, fields: fields, id:"id_Medico" },
-      href: "/viewmedico", 
+      href: "/viewmedico",
       page_title: "Medicos"
     };
     res.render("table_view", locals, function(err, html){
