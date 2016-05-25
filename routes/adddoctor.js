1cecede757297d751a6f6d28c9d1a6494774bdc0
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2016-05-25 02:02:22 -0700

    ya valida que no se inserte el mismo wey dos veces

--- routes/adddoctor.js
+++ routes/adddoctor.js
@@ -8,25 +8,32 @@ router.get('/', function(req, res, next) {
   res.render('create_doctor');
 });
 router.post('/', function(req, res, next) {
-  var sql = "INSERT INTO persona (nombre,apellido_paterno,apellido_materno,tipo) VALUES (?,?,?,?)";
-  var wheres = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno,'medico'];
-  var query1 = req.app.mysql.format(sql, wheres);
-  console.log(query1);
-  req.app.mysql.query(query1, function(err2, rows2, fields2){
-    var sql2 = "SELECT id_Persona FROM persona WHERE nombre=? AND apellido_paterno=? AND apellido_materno=?";
-    var val = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno];
-    var query2 = req.app.mysql.format(sql2, val);
-    console.log(query2);
-    req.app.mysql.query(query2, function(err3, rows3, fields3){
-        if(rows3.length == 1){
-          var sql3 = "INSERT INTO medico (id_Persona,especialidad,cedula_profesional) VALUES (?,?,?)";
-          var valu = [rows3[0].id_Persona, req.body.esp,req.body.cedula];
-          var query3 = req.app.mysql.format(sql3, valu);
-          console.log(query3);
-          req.app.mysql.query(query3, function(err4, rows4, fields4){
-          });
-        }
-    });
+  var consult = "SELECT id_Persona FROM persona WHERE nombre=? AND apellido_paterno=? AND apellido_materno=? AND tipo=?"
+  var wher = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno,'medico'];
+  var query0 = req.app.mysql.format(consult, wher);
+  req.app.mysql.query(query0, function(err, rows, fields){
+    if(rows.length == 0){
+      var sql = "INSERT INTO persona (nombre,apellido_paterno,apellido_materno,tipo) VALUES (?,?,?,?)";
+      var wheres = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno,'medico'];
+      var query1 = req.app.mysql.format(sql, wheres);
+      console.log(query1);
+      req.app.mysql.query(query1, function(err2, rows2, fields2){
+        var sql2 = "SELECT id_Persona FROM persona WHERE nombre=? AND apellido_paterno=? AND apellido_materno=?";
+        var val = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno];
+        var query2 = req.app.mysql.format(sql2, val);
+        console.log(query2);
+        req.app.mysql.query(query2, function(err3, rows3, fields3){
+            if(rows3.length == 1){
+              var sql3 = "INSERT INTO medico (id_Persona,especialidad,cedula_profesional) VALUES (?,?,?)";
+              var valu = [rows3[0].id_Persona, req.body.esp,req.body.cedula];
+              var query3 = req.app.mysql.format(sql3, valu);
+              console.log(query3);
+              req.app.mysql.query(query3, function(err4, rows4, fields4){
+              });
+            }
+        });
+      });
+    }
   });
   res.redirect("/");
 });
--- routes/addpaciente.js
+++ routes/addpaciente.js
@@ -9,25 +9,32 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
-  var sql = "INSERT INTO persona (nombre,apellido_paterno,apellido_materno,tipo) VALUES (?,?,?,?)";
-  var wheres = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno,'paciente'];
-  var query1 = req.app.mysql.format(sql, wheres);
-  console.log(query1);
-  req.app.mysql.query(query1, function(err2, rows2, fields2){
-    var sql2 = "SELECT id_Persona FROM persona WHERE nombre=? AND apellido_paterno=? AND apellido_materno=?";
-    var val = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno];
-    var query2 = req.app.mysql.format(sql2, val);
-    console.log(query2);
-    req.app.mysql.query(query2, function(err3, rows3, fields3){
-        if(rows3.length == 1){
-          var sql3 = "INSERT INTO paciente (id_Persona,entidad_serv_salud,eps) VALUES (?,?,?)";
-          var valu = [rows3[0].id_Persona, req.body.ess,req.body.eps];
-          var query3 = req.app.mysql.format(sql3, valu);
-          console.log(query3);
-          req.app.mysql.query(query3, function(err4, rows4, fields4){
-          });
-        }
-    });
+  var consult = "SELECT id_Persona FROM persona WHERE nombre=? AND apellido_paterno=? AND apellido_materno=? AND tipo=?"
+  var wher = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno,'paciente'];
+  var query0 = req.app.mysql.format(consult, wher);
+  req.app.mysql.query(query0, function(err, rows, fields){
+    if(rows.length == 0){
+      var sql = "INSERT INTO persona (nombre,apellido_paterno,apellido_materno,tipo) VALUES (?,?,?,?)";
+      var wheres = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno,'paciente'];
+      var query1 = req.app.mysql.format(sql, wheres);
+      console.log(query1);
+      req.app.mysql.query(query1, function(err2, rows2, fields2){
+        var sql2 = "SELECT id_Persona FROM persona WHERE nombre=? AND apellido_paterno=? AND apellido_materno=?";
+        var val = [req.body.nombre, req.body.apellidopaterno,req.body.apellidomaterno];
+        var query2 = req.app.mysql.format(sql2, val);
+        console.log(query2);
+        req.app.mysql.query(query2, function(err3, rows3, fields3){
+            if(rows3.length == 1){
+              var sql3 = "INSERT INTO paciente (id_Persona,entidad_serv_salud,eps) VALUES (?,?,?)";
+              var valu = [rows3[0].id_Persona, req.body.ess,req.body.eps];
+              var query3 = req.app.mysql.format(sql3, valu);
+              console.log(query3);
+              req.app.mysql.query(query3, function(err4, rows4, fields4){
+              });
+            }
+        });
+      });
+    }
   });
   res.redirect("/");
 });
