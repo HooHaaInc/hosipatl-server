f3819f80d179a2e5be0b3dfb19d3800627819405
Nan <nano.mv112@gmail.com>
2016-05-24 19:58:16 -0700

    views guapos

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 var express = require('express');
 var session = require('express-session')
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
