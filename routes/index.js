23a9abfe8432647a5cc731698e299c7b3f574349
Nan <nano.mv112@gmail.com>
2016-05-26 11:52:05 -0700

    inicio en pacientes

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(!req.session.email || !req.session.password)
-    res.redirect('../login');
-  else res.render("index");
+    res.redirect('/login');
+  else res.redirect('/tablepacientes');
 });
 
 module.exports = router;
